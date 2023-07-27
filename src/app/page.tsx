9cb8cfe0e1352461ca1b3c08f97aacc9bc912cf5
tortrakul-won <tortrakulwonglieng@gmail.com>
2023-07-27 15:23:04 +0700

    clean up 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,27 @@
 "use client";
 
 import Sources from "@/components/Sources";
-import PopulationGrowth from "../components/PopulationGrowth/PopulationGrowth";
+import PopulationGrowth from "../components/PopulationGrowth";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col align-middle items-center md:mx-[20vw] mx-[10vw]">
-      <span className="text-white text-[28px] font-bold my-3 flex-1 w-full">
-        สถิติประชากรกรุงเทพฯ พ.ศ. 2550 - 2559
-      </span>
-      <span className="text-white text-[18px] font-bold my-3 flex-1 w-full">
-        ลักษณะพื้นที่
-      </span>
+      <span className="text-white text-[28px] font-bold my-3 flex-1 w-full">สถิติประชากรกรุงเทพฯ พ.ศ. 2550 - 2559</span>
+      <span className="text-white text-[18px] font-bold my-3 flex-1 w-full">ลักษณะพื้นที่</span>
       <span className="text-white text-[14px] my-3">
-        กรุงเทพฯ เป็นจังหวัดที่มีประชากรมากที่สุดในประเทศไทย
-        หากรวมประชากรแฝงที่ไม่ปรากฏในทะเบียนและคนที่
-        เดินทางมาทำงานในตอนกลางวันด้วยแล้ว
-        คาดว่าจะสูงถึงเกือบเท่าตัวของประชากรที่ปรากฏในทะเบียน เราจึง
-        เรียกกรุงเทพฯ ว่าเป็น{" "}
+        กรุงเทพฯ เป็นจังหวัดที่มีประชากรมากที่สุดในประเทศไทย หากรวมประชากรแฝงที่ไม่ปรากฏในทะเบียนและคนที่
+        เดินทางมาทำงานในตอนกลางวันด้วยแล้ว คาดว่าจะสูงถึงเกือบเท่าตัวของประชากรที่ปรากฏในทะเบียน เราจึง เรียกกรุงเทพฯ
+        ว่าเป็น{" "}
         {
-          <Link
-            href={"https://en.wikipedia.org/wiki/Megacity"}
-            target="_blank"
-            className="underline"
-          >
+          <Link href={"https://en.wikipedia.org/wiki/Megacity"} target="_blank" className="underline">
             “อภิมหานคร (megacity)”
           </Link>
         }{" "}
         คือมีประชากรตั้งแต่ 10 ล้านคนขึ้นไป
         <br />
         <br />
-        <p>
-          อัตราเพิ่มของประชากรกรุงเทพฯ อยู่ระดับเกือบ 1% และเริ่มลดลงในปี 2559
-          ดังแสดงในแผนภูมิต่อไปนี้
-        </p>
+        <p>อัตราเพิ่มของประชากรกรุงเทพฯ อยู่ระดับเกือบ 1% และเริ่มลดลงในปี 2559 ดังแสดงในแผนภูมิต่อไปนี้</p>
       </span>
       <PopulationGrowth />
       <Sources />
